bacfe865c910161b550257a40b950a469d89e04f
FateevNIX <aleksandr.fateev@nixsolutions.com>
2019-11-04 15:48:44 +0200

    fixed selectors and changed logic of some methods

--- src/test/java/com/amazon/Pages/ProductDetailsPage.java
+++ src/test/java/com/amazon/Pages/ProductDetailsPage.java
@@ -4,18 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductDetailsPage {
     WebDriver driver;
-    By labelToSelectSize = By.cssSelector("//*[@id=\"partialStateBuybox\"]/div/div[1]/span/text()");
-    By addToCartButton = By.cssSelector("span > span[id*=add-to-cart]");
-    By selectSizeDropdownFirstOption = By.cssSelector("//*[@class=\"a-popover a-dropdown a-dropdown-common a-declarative\"]//li[2]");
+    By labelToSelectSize = By.xpath("//span[contains(text(),'To buy, select')]");
+    By addToCartButton = By.id("add-to-cart-button");
+    By dropdownWithSizeOptions = By.id("dropdown_selected_size_name");
+    By selectSizeDropdownFirstOption = By.xpath("//li[contains(@id,'size_name') and contains(@class,'dropdownAvailable')]");
+    By inStockLabel = By.xpath("//*[@id='availability']/span");
 
     public ProductDetailsPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public Boolean checkIfUserNeedToChooseSizeOfProduct(WebDriver driver) {
+    public Boolean checkIfUserNeedToChooseSizeOfProduct(WebDriver driver) { //этот метод увеличивает шанс прохождения теста, на шаге когда надо добавить товар в корзину
         Boolean triger = false;
         try {
             driver.findElement(labelToSelectSize).isDisplayed();
@@ -26,14 +30,14 @@ public class ProductDetailsPage {
         return triger;
     }
 
-    public void clickOnAddToCartButton(){ //наверное лучше это разбить на два метода, если надо исправлю
-        WebElement addCartButton = driver.findElement(addToCartButton);
+    public void clickOnAddToCartButton(){
             if (checkIfUserNeedToChooseSizeOfProduct(driver)) {
-                addCartButton.click(); }
+                driver.findElement(addToCartButton).click(); }
             else{
-                  WebElement selectSizeOfProduct = driver.findElement(selectSizeDropdownFirstOption);
-                  selectSizeOfProduct.click();
-                  addCartButton.click();
+                  driver.findElement(dropdownWithSizeOptions).click();
+                  driver.findElement(selectSizeDropdownFirstOption).click();
+                  new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(inStockLabel));
+                  driver.findElement(addToCartButton).click();
             }
     }
 }
